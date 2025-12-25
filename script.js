e8ff92def229a0d2ee3327ae1b4b13a6e6337864
Tihomir13 <tihomir13n2002@abv.bg>
2025-12-25 19:18:19 +0200

    optimise js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const header = document.querySelector('.header');
 const currentYear = new Date().getFullYear();
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    header.classList.add('shadow');
-  } else {
-    header.classList.remove('shadow');
-  }
-});
+window.addEventListener(
+  'scroll',
+  () => {
+    if (window.scrollY > 0) {
+      header.classList.add('shadow');
+    } else {
+      header.classList.remove('shadow');
+    }
+  },
+  { passive: true }
+);
 
 function updateYear() {
   const el = document.querySelector('.year');
